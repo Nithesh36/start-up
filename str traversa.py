7bced2458211821bec47b94660ffa3d82d9ce7ab
NITHESH-12 <64261679+NITHESH-12@users.noreply.github.com>
2020-04-28 22:51:45 +0530

    Add files via upload

---
+++ str traversa.py
@@ -0,0 +1,21 @@
+#Traversal and the for Loop: By Index¶
+fruit = "apple"
+for idx in range(len(fruit)):
+    print(fruit[idx])#we need a temporary variable to print the  specified index value in the string
+    #here  we doesnot use so it will print all the values one  by one or iterate
+
+
+fruit = "apple"
+re=int(input("enter a range\n"))
+for idx in range(re):#suppose the value is 2
+#output is a,p ..here the val is index position fruit[0]
+#fruit[1] can be printed  ...(a,p)is the result
+   print(fruit[idx])
+
+#here we can print a value in line by line based on given range
+
+
+
+#output 
+a
+p
